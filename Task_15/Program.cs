74b8cfac6f51e60ebf2e8fd2e582f4a0a6d6de2c
Pantokha Alexander <ealex47@gmail.com>
2022-12-09 19:06:19 +0300

    решил третью задачу

---
+++ Task_15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine(" Какой сегодня день недели по счету? ");
+int Date = int.Parse(Console.ReadLine());
+if (Date < 6)
+{
+    Console.WriteLine(" Сегодня будний день :( ");
+}
+if (Date >= 6)
+{
+    Console.WriteLine(" Ура!!! Сегодня выходой :) ");
+}
