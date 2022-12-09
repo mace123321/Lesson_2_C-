7266a27c29f76f357232604e8b5d8df4b0119aa5
Pantokha Alexander <ealex47@gmail.com>
2022-12-09 18:16:26 +0300

    Сделал первую задачу

---
+++ Task_10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите любое число от 100 до 999:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum1 = number / 10;
+int FinalSum = sum1 % 10;
+Console.WriteLine($"{FinalSum}");
